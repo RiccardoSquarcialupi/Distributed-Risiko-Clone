293fab33b0eb4719616ce510570f1b22c031ce8e
Filippo Benvenuti <filippo.benvenuti3@studio.unibo.it>
2022-02-28 18:44:20 +0100

    Upload basic structure of webServer

--- WebServer/src/app.py
+++ WebServer/src/app.py
@@ -8,5 +8,15 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/home', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API.</p>"
+
+
+@app.route('/product/<name>')
+def get_product(name):
+    return "The product is " + str(name)
+
+
 if __name__ == '__main__':
     app.run()
