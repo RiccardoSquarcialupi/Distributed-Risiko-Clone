4760db88ea201f8e46d52cf518bad7cff8e6b540
Filippo Benvenuti <filippo.benvenuti3@studio.unibo.it>
2023-10-02 11:18:48 +0200

    added GUILobby basic GUI

--- lobby/src/main/java/app/lobby/GUI/GUILobby.java
+++ lobby/src/main/java/app/lobby/GUI/GUILobby.java
@@ -4,9 +4,36 @@ import app.manager.gui.GUI;
 
 import javax.swing.*;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 public class GUILobby extends JPanel implements GUI, GUILobbyActions {
     @Override
     public String getTitle() {
         return "RiSiKo!!! Lobby Pre Game";
+    }
+
+    private DefaultListModel<String> playersListModel;
+    private JList<String> playersList;
+    private JLabel countLabel;
+    private JButton removeButton;
+
+    public GUILobby() {
+        setLayout(new BorderLayout());
+
+        playersListModel = new DefaultListModel<>();
+        playersList = new JList<>(playersListModel);
+        JScrollPane listScrollPane = new JScrollPane(playersList);
+
+        countLabel = new JLabel("Participants: 1");
+        removeButton = new JButton("Rimuovi Persona");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.add(removeButton);
+
+        add(listScrollPane, BorderLayout.CENTER);
+        add(countLabel, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 }
