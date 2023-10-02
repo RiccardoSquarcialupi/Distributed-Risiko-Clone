d3348ffd940ae888177f3254e5438b11d1192167
Riccardo <riccard.squarcialupi@studio.unibo.it>
2023-10-02 14:59:33 +0200

    added a first design for the lobby pre game, BUG FOUND: white screen disappear only when the window is iconized and deiconized, probably a swing bug...

--- lobby/src/main/java/app/lobby/GUI/GUILobby.java
+++ lobby/src/main/java/app/lobby/GUI/GUILobby.java
@@ -4,8 +4,9 @@ import app.manager.gui.GUI;
 
 import javax.swing.*;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.*;
+import java.io.File;
+import java.util.Objects;
 
 public class GUILobby extends JPanel implements GUI, GUILobbyActions {
     @Override
@@ -13,27 +14,38 @@ public class GUILobby extends JPanel implements GUI, GUILobbyActions {
         return "RiSiKo!!! Lobby Pre Game";
     }
 
+    private JLabel loadingLabel;
     private DefaultListModel<String> playersListModel;
     private JList<String> playersList;
-    private JLabel countLabel;
-    private JButton removeButton;
+    private JButton exitButton;
+
 
     public GUILobby() {
         setLayout(new BorderLayout());
+        setBackground(new Color(0x2E3842));
 
-        playersListModel = new DefaultListModel<>();
+        JPanel titlePanel = new JPanel();
+        JLabel titleLabel = new JLabel("Please Wait for the game to start...");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titlePanel.add(titleLabel);
+        titlePanel.setBackground(new Color(0x2E3842));
+        titleLabel.setForeground(new Color(0xF7DC6F));
+
+
+        // Loading Image
+        ImageIcon imgIcon = new ImageIcon("/assets/image/RisikoLoading.gif");
+        loadingLabel = new JLabel(imgIcon);
+        add(loadingLabel, BorderLayout.CENTER);
+
+
+        // Players List
+        /*playersListModel = new DefaultListModel<>();
         playersList = new JList<>(playersListModel);
-        JScrollPane listScrollPane = new JScrollPane(playersList);
+        JScrollPane playersScrollPane = new JScrollPane(playersList);
+        add(playersScrollPane, BorderLayout.NORTH);*/
 
-        countLabel = new JLabel("Participants: 1");
-        removeButton = new JButton("Rimuovi Persona");
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout());
-        buttonPanel.add(removeButton);
-
-        add(listScrollPane, BorderLayout.CENTER);
-        add(countLabel, BorderLayout.NORTH);
-        add(buttonPanel, BorderLayout.SOUTH);
+        // Exit Button
+        exitButton = new JButton("Exit Lobby");
+        add(exitButton, BorderLayout.SOUTH);
     }
 }
